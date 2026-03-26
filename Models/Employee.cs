886d0afad91b0118c2e0569b89ecdc02e343e360
Akhila Reddy <akhila.dodla@nexergroup.com>
2026-03-26 15:19:50 +0530

    Clean main branch

--- Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace AssetManagementApi.Models;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string EmployeeId { get; set; } = string.Empty;
-}
